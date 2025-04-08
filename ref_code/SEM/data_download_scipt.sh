b02bea5749f19c1a4f83ffa7dbca7e0f50b0c972
Huynh Thanh Tan <tan.huynh@anfin.vn>
2025-04-08 08:39:40 +0700

    chore: update scripts

--- ref_code/SEM/data_download_scipt.sh
+++ ref_code/SEM/data_download_scipt.sh
@@ -6,13 +6,16 @@ pip install -q gdown
 # File ID and output filename
 FILE_ID="1z3y-0lSEadWgo5mI4uaijBPc_Ts-dbSB"
 OUTPUT="downloaded_file.zip"
-TARGET_DIR="/dataset"
+TARGET_DIR="./dataset"
 
 # Download the file
 gdown "https://drive.google.com/uc?id=${FILE_ID}" -O "${OUTPUT}"
 
 # Create target directory if it doesn't exist
 mkdir -p "${TARGET_DIR}"
+
+# For the ubuntu root
+# apt install -y unzip
 
 # Unzip into the target folder
 unzip -q "${OUTPUT}" -d "${TARGET_DIR}"
